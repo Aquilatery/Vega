9d6d602c62841722e01c08a5bd2abee0cc67add0
Taiizor <41683699+Taiizor@users.noreply.github.com>
2022-03-24 23:56:30 +0300

    .NET7 Support

--- src/Vega/Vega.cs
+++ src/Vega/Vega.cs
@@ -14,7 +14,7 @@ namespace Vega
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-#if NETCOREAPP3_1 || NET5_0 || NET6_0
+#if NETCOREAPP3_1 || NET5_0 || NET6_0 || NET7_0
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
 #endif
 
